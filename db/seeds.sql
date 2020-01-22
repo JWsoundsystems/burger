08c4dbf814a37e2c685e63f3735b38ccfc730958
JWsoundsystems <JWsoundsystems@gmail.com>
2020-01-21 22:34:19 -0800

    Set up MySQL

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+SELECT * FROM burgerDB.burgers;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Whopper", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Big Mac", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Jumbo Jack", true);
